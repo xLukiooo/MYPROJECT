bfce0be1b4d3a4c3fcceaa7ac057ded292250a0e
xLukiooo <128417655+xLukiooo@users.noreply.github.com>
2025-03-23 12:28:09 +0100

    Update settings.py

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -76,7 +76,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 """
